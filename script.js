4ea1118b8bca67d84a17ecbe9314b582655e7899
Vaibhav Shukla <vaibhavshuklaofficial8586@gmail.com>
2026-01-06 20:23:00 +0530

    Use relative API path for Azure preview environments

--- script.js
+++ script.js
@@ -25,7 +25,20 @@ btn.addEventListener("click", () => {
 });
 
 function sendEmergency(latitude, longitude) {
-    fetch("https://liferoute-api-gdhwdhcdffhcenbh.centralindia-01.azurewebsites.net/api/emergency", {
+    // working API Call for MVP-v1
+    // fetch("https://liferoute-api-gdhwdhcdffhcenbh.centralindia-01.azurewebsites.net/api/emergency", {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //         latitude: latitude,
+    //         longitude: longitude
+    //     })
+    // })
+    
+    //------MVP-v2 API Call -----------------------------------------|
+        fetch("/api/emergency", {   // ✅ FIXED (relative path)
         method: "POST",
         headers: {
             "Content-Type": "application/json"
